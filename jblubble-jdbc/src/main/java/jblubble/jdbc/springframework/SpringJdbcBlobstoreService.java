4de9115e24a6890bce0f3edd13e6f3f307f5ed5b
Lorenzo Dee <lorenzo.dee@orangeandbronze.com>
2015-10-06 15:31:07 +0800

    Adding license header

--- jblubble-jdbc/src/main/java/jblubble/jdbc/springframework/SpringJdbcBlobstoreService.java
+++ jblubble-jdbc/src/main/java/jblubble/jdbc/springframework/SpringJdbcBlobstoreService.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package jblubble.jdbc.springframework;
 
 import java.io.IOException;
@@ -25,13 +40,15 @@ import jblubble.BlobstoreWriteCallback;
 import jblubble.jdbc.AbstractJdbcBlobstoreService;
 
 /**
- * An experimental {@link BlobstoreService blobstore service} implementation
- * using {@link JdbcTemplate} in Spring Framework. Using {@link JdbcTemplate
- * JDBC template} translates JDBC exceptions to {@link DataAccessException}s.
+ * A {@link BlobstoreService blobstore service} implementation using
+ * {@link JdbcTemplate} of the Spring Framework. Using {@link JdbcTemplate JDBC
+ * template} translates JDBC exceptions to {@link DataAccessException}s.
  * <p>
  * This has the added advantage of inherently being able to participate in
  * Spring-managed transactions.
- *
+ * </p>
+ * 
+ * @author Lorenzo Dee
  */
 public class SpringJdbcBlobstoreService extends AbstractJdbcBlobstoreService {
 
